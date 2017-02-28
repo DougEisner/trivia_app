d0900120a8ca3bbca5a9893798993c01a18e5778
Amiee <amiee.marielo@gmail.com>
2017-02-28 17:13:29 -0500

    setup for rails-angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,13 @@
 (function(ng) {
-  ng.module('TriviaApp', ['ui.router', 'templates']);
+  ng.module('TriviaApp', ['ui.router', 'templates', 'LocalStorageModule']);
 
   ng.module('TriviaApp').config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('home');
 
-    $stateProvider.state('home', {
+    $stateProvider.state('home', { // ParentState, abstract true, state. ParentState.index (move ui-view to index folder)
       url: '/home',
-      templateUrl: 'main/_home.html',
-      controller: 'MainController'
+      templateUrl: 'main/_home.html', // can move to template folder
+      controller: 'MainController' // views/ home /index.html.erb <ui-view></ui-view>
     });
   });
 
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module TriviaApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+        g.javascript_engine :js
+    end
+
+    config.assets.paths << Rails.root.join('vendor', 'assets' 'bower_components')
   end
 end
