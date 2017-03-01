a46f2300ac8fdc1902fe8b563aeb97d22f9c0f50
Scott Hsu <scotthsumail@yahoo.com>
2017-03-01 14:42:46 -0500

    added mockups and css

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,15 @@
 (function(ng) {
-  ng.module('TriviaApp', ['ui.router', 'templates', 'LocalStorageModule']);
+    ng.module('TriviaApp', ['ui.router', 'templates', 'LocalStorageModule']);
 
-  ng.module('TriviaApp').config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('home');
+    ng.module('TriviaApp').config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('home');
 
-    $stateProvider.state('home', { // ParentState, abstract true, state. ParentState.index (move ui-view to index folder)
-      url: '/home',
-      templateUrl: '_home.html', // can move to template folder
-      controller: 'LoginController as login' // views/ home /index.html.erb <ui-view></ui-view>
+        $stateProvider.state('home', { // ParentState, abstract true, state. ParentState.index (move ui-view to index folder)
+            url: '/home',
+            templateUrl: '_home.html', // can move to template folder
+            controller: 'LoginController as login' // views/ home /index.html.erb <ui-view></ui-view>
+        });
     });
-  });
 
 
 })(angular);
