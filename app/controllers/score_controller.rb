574d3850f147b4857bd3b866c2877f0eeadcdac1
jnmerchant <jnmercnc@gmail.com>
2017-03-02 13:09:01 -0500

    added validation to score model

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -5,6 +5,16 @@ class ScoreController < ApplicationController
   end
 
   def create
+    @score = Score.new(score_params)
 
+    if @score.save
+      render json: @score, status: :created, location: @score
+    else
+      render json: @score.errors, status: :unprocessable_entity
+    end
   end
+
+  def score_params
+      params.require(:score).permit(:score, :user_id)
+    end
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,4 @@
 class Score < ApplicationRecord
+  validates :user_id, :score, presence: true
   belongs_to :user
 end
