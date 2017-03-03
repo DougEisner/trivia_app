22dc0946533e79095ceb72938bf4a49e02a99c6f
Scott Hsu <scotthsumail@yahoo.com>
2017-03-03 08:23:47 -0500

    Doody doc css changes

--- app/assets/javascripts/controllers/login-controller.js
+++ app/assets/javascripts/controllers/login-controller.js
@@ -1,59 +1,60 @@
 (function(ng) {
-  ng.module('TriviaApp').controller('LoginController', function($state, localStorageService, $scope, DataRequestService, $q) {
-      console.log('in c');
+    ng.module('TriviaApp').controller('LoginController', function($state, localStorageService, $scope, DataRequestService, $q) {
+        console.log('in c');
 
-      $scope.userInfo = []; // store userInfo array
-
-
-
-    //   $q.when(DataRequestService.post('')).then((response) => {
-    //          console.log(response);
-    //          this.allQuestions = response.data; // set the response to the allQuestions Array?
-    //          getAnswer(this.allQuestions.whatever); /// maybe this instead?
-    //          console.log(this.allQuestions);
-    //      }).catch((error) => {
-    //          console.log(error);
-    //      });
-
-    // SEND DATA OF USERINFO TO POST REQUEST
+        $scope.userInfo = []; // store userInfo array
 
 
-    // $('.toggle-link').on('click', function() {
-    //     console.log('in');
-    // });
+        //   $q.when(DataRequestService.post('')).then((response) => {
+        //          console.log(response);
+        //          this.allQuestions = response.data; // set the response to the allQuestions Array?
+        //          getAnswer(this.allQuestions.whatever); /// maybe this instead?
+        //          console.log(this.allQuestions);
+        //      }).catch((error) => {
+        //          console.log(error);
+        //      });
+
+        // SEND DATA OF USERINFO TO POST REQUEST
+        // Start Scooters stuff
+
+        $scope.signup = function(item) {
+            $scope.selected = item;
+        };
 
 
-      $scope.inputInfo = { // inputinfo obj
-          userName: '',
-          passWord: '',
-          email: '',
-          imageUrl: ''
-      };
 
-      // this.logout = function() { // DELETE REQUEST
-      //     this.inputInfo.userName = null;
-      //     this.inputInfo.passWord = null;
-      //     $location.path('/'); $state.go('TriviaParent.login'); http://localhost:3000/#!/login
-      // };
 
-      $scope.submit = function() { // submit function
+        $scope.inputInfo = { // inputinfo obj
+            userName: '',
+            passWord: '',
+            email: '',
+            imageUrl: ''
+        };
 
-        //   console.log($scope.userInfo);
-          $scope.userInfo.push($scope.inputInfo); // push inputInfo in userInfo array
-          $scope.setInfo($scope.userInfo); // pass userInfo in setInfo function
-          $scope.userinfo = $scope.getInfo(); // set userinfo to getInfo function
-        //   console.log($scope.getInfo()[0].userName);
-          console.log($scope.userInfo);
-      };
+        // this.logout = function() {
+        //     this.inputInfo.userName = null;
+        //     this.inputInfo.passWord = null;
+        //     $location.path('/');
+        // };
 
-      $scope.setInfo = function(userInfo) { // set local storage
-      localStorageService.set('userInfo', $scope.userInfo);
-    };
+        $scope.submit = function() { // submit function
 
-      $scope.getInfo = function() { // get local storage
-          return localStorageService.get('userInfo') || [];
-    };
+            //   console.log($scope.userInfo);
+            $scope.userInfo.push($scope.inputInfo); // push inputInfo in userInfo array
+            $scope.setInfo($scope.userInfo); // pass userInfo in setInfo function
+            $scope.userinfo = $scope.getInfo(); // set userinfo to getInfo function
+            //   console.log($scope.getInfo()[0].userName);
+            console.log($scope.userInfo);
+        };
 
-  });
+        $scope.setInfo = function(userInfo) { // set local storage
+            localStorageService.set('userInfo', $scope.userInfo);
+        };
+
+        $scope.getInfo = function() { // get local storage
+            return localStorageService.get('userInfo') || [];
+        };
+
+    });
 
 })(angular);
